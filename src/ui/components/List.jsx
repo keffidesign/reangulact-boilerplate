8a4536ae9ba0e078f173c89f654d5be4ff4a6110
Igor <keffidesign@gmail.com>
2016-03-15 15:26:17 +0300

    fix list

--- src/ui/components/List.jsx
+++ src/ui/components/List.jsx
@@ -7,14 +7,16 @@ export default class UiList extends DataComponent {
     };
 
     static TEMPLATE = (
-        <ul class='list-group' if=':data'>
-            <li class='list-group-item' each='datum of :data'>
-                :datum.name
-            </li>
-            <else if=':error'>
-                <span>:Error: :error.message</span>
-            </else>
-        </ul>
+        <div>
+            <ul class='list-group' if=':data'>
+                <li class='list-group-item' each='datum of :data'>
+                    :datum.name
+                </li>
+                <else if=':error'>
+                    <span>:Error: :error.message</span>
+                </else>
+            </ul>
+        </div>
     );
 
 }
